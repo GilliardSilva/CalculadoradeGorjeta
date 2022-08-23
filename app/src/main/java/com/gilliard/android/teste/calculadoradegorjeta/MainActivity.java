8e7eb41c502f999e42696359d5df71d630f51125
Gilliard Silva <gilliard-cart@hotmail.com>
2022-08-23 19:27:01 -0300

    Primeiro Commit

---
+++ app/src/main/java/com/gilliard/android/teste/calculadoradegorjeta/MainActivity.java
@@ -0,0 +1,82 @@
+package com.gilliard.android.teste.calculadoradegorjeta;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editValor;
+    private TextView textPorcentagem;
+    private TextView textGorjeta;
+    private TextView textTotal;
+    private SeekBar seekBarGorjeta;
+
+    private double porcentagem = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editValor       = findViewById(R.id.editValor);
+        textPorcentagem = findViewById(R.id.textPorcentagem);
+        textGorjeta     = findViewById(R.id.textGorjeta);
+        textTotal       = findViewById(R.id.textTotal);
+        seekBarGorjeta  = findViewById(R.id.seekBarGorjeta);
+
+        //Adicionar o listener SeekBar
+        seekBarGorjeta.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+                porcentagem = i;
+                textPorcentagem.setText(Math.round(porcentagem) + " %");
+                calcular();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+
+    public void calcular(){
+
+        String valorRecuperado = editValor.getText().toString();
+        if(valorRecuperado == null || valorRecuperado.equals("")){
+
+            Toast.makeText(
+                    getApplicationContext(),
+                    "Digite o valor primeiro!",
+                    Toast.LENGTH_SHORT
+            ).show();
+        } else{
+
+            //Converter string para double
+            double valorDigitado = Double.parseDouble(valorRecuperado);
+
+            //Calcular a gorjeta
+            double gorjeta = valorDigitado * (porcentagem / 100);
+            double total = gorjeta + valorDigitado;
+
+            //Exibir a gorjeta e o total
+            //textGorjeta.setText("R$ " + Math.round(gorjeta));
+            textGorjeta.setText("R$ " + gorjeta);
+            textTotal.setText("R$ "+ total);
+        }
+
+
+    }
+}
